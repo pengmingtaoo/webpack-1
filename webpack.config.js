442336278999c1944ab2525026d1edfde62af207
pengmingtao <pengmingtao2022@163.com>
2022-07-14 21:20:41 +0800

    使用了5.73.0版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
-module.exports = {
+
+  module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+      filename: '[name].[contenthash].js',
     },
-   
-    };
+};
